63c150cd1bdaa4f58c7e868776fa523e27f3740e
shibly-mhd <shibilik26075@gmail.com>
2024-07-18 12:26:56 +0530

    Add files via upload

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let a=prompt("please enter first number");
+
+// let b=prompt("please enter second number");
+// let c=parseInt(a)+parseInt(b);
+
+// console.log("result of "+a+" and "+b+" is :"+c);
